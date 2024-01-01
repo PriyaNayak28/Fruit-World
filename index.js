7368d4a697289c6e395a19dabeeb18c4ad4d54a1
PriyaNayak28 <nayakpriya612@gmail.com>
2024-01-01 12:20:36 +0530

    change somelines

---
+++ index.js
@@ -0,0 +1,16 @@
+const mainheader = document.getElementById('main-heading');
+mainheader.textContent = 'Fruit World';
+mainheader.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '4px solid orange';
+
+const basketheading = document.getElementById('basket-heading');
+basketheading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.textContent = 'Please visit us again';
+
+
+
