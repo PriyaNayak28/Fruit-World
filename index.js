04d42aa9d3cde8104eb203d07430a45ef22ac809
PriyaNayak28 <nayakpriya612@gmail.com>
2024-01-03 21:38:26 +0530

    adding btn for add fruits

--- index.js
+++ index.js
@@ -55,7 +55,7 @@
 // fruits.style.width = '50%';
 
 
-// querySelectorAll  is  used to grab  all  Elements with class/tag 
+// querySelectorAll  is  used to grab  all  Elements with class/tag
 
 // const fruit = document.querySelectorAll('.fruit');
 
@@ -74,42 +74,68 @@
 
 // createElement  domrelation
 
-const head = document.createElement('h3');
-const headPara = document.createTextNode('Buy high quality organic fruits online');
+// const head = document.createElement('h3');
+// const headPara = document.createTextNode('Buy high quality organic fruits online');
 
-head.appendChild(headPara);
+// head.appendChild(headPara);
 
-head.style.fontStyle = 'italic';
+// head.style.fontStyle = 'italic';
 
-const divs = document.getElementsByTagName('div');
-const FirstDiv = divs[0];
+// const divs = document.getElementsByTagName('div');
+// const FirstDiv = divs[0];
 
-FirstDiv.appendChild(head);
+// FirstDiv.appendChild(head);
 
-const para = document.createElement('p');
-const ParaText = document.createTextNode('Total fruits: 4');
-para.appendChild(ParaText);
+// const para = document.createElement('p');
+// const ParaText = document.createTextNode('Total fruits: 4');
+// para.appendChild(ParaText);
 
-const SecDiv = divs[1];
-// const fruits = document.getElementsByClassName('')
-const basketHeading = document.getElementById('basket-heading');
-SecDiv.insertBefore(para, basketHeading);
+// const SecDiv = divs[1];
+// // const fruits = document.getElementsByClassName('')
+// const basketHeading = document.getElementById('basket-heading');
+// SecDiv.insertBefore(para, basketHeading);
 
-para.id = 'fruits-total';
-
-
+// para.id = 'fruits-total';
 
 
+// ADD AND DELETE FRUITS.................................................. 
+
+// const form = document.querySelector('form');
+// const fruitsContainer = document.querySelector('.fruits');
+
+// form.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     const fruitToAdd = document.getElementById('fruit-to-add');
+//     const newLi = document.createElement('li');
+//     const newLiText = document.createTextNode(fruitToAdd.value);
+//     newLi.appendChild(newLiText);
+//     newLi.className = 'fruit';
+//     const deleteBtn = document.createElement('button');
+//     const deleteBtnText = document.createTextNode('x');
+//     deleteBtn.appendChild(deleteBtnText);
+//     deleteBtn.className = 'delete-btn';
+//     newLi.appendChild(deleteBtn);
+//     fruitsContainer.appendChild(newLi);
+// });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    const fruitsContainer = document.querySelector('.fruits');
 
-
-
-
-
-
-
-
-
-
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const fruitToAdd = document.getElementById('fruit-to-add');
+        const newLi = document.createElement('li');
+        const newLiText = document.createTextNode(fruitToAdd.value);
+        newLi.appendChild(newLiText);
+        newLi.className = 'fruit';
+        const deleteBtn = document.createElement('button');
+        const deleteBtnText = document.createTextNode('x');
+        deleteBtn.appendChild(deleteBtnText);
+        deleteBtn.className = 'delete-btn';
+        newLi.appendChild(deleteBtn);
+        fruitsContainer.appendChild(newLi);
+    });
+});
 
